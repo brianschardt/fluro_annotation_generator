1e8472d5e37e565b5a7076eb9a86954b7fe770e1
brian schardt <brianschardt@brians-MacBook-Pro.local>
2020-05-02 14:51:56 -0700

    added library name

--- fluro_annotation_generator_fork/lib/fluro_annotation_generator_fork.dart
+++ fluro_annotation_generator_fork/lib/fluro_annotation_generator_fork.dart
@@ -1,4 +1,4 @@
-library fluro_annotation_generator;
+library fluro_annotation_generator_fork;
 
 export 'package:fluro_annotation_generator_fork/src/annotation/f_route.dart';
 export 'package:fluro_annotation_generator_fork/src/annotation/enable_fluro_router.dart';
